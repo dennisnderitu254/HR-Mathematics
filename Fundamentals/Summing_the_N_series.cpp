92feeb76e7dbd7eb91a6d6cd86b44ed73eda99ed
Hiroyuki Hirano <aitdccd@gmail.com>
2018-04-24 20:04:50 +0900

    Summing the N series

--- Fundamentals/Summing_the_N_series.cpp
+++ Fundamentals/Summing_the_N_series.cpp
@@ -1,19 +1,28 @@
+// https://www.hackerrank.com/challenges/summing-the-n-series/problem
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
-#define ull unsigned long long
-const ull MOD = pow(10, 9) + 7;
+const long long MOD = pow(10, 9) + 7;
 
 int main() {
-    ios::sync_with_stdio(false);
     int t;
     cin >> t;
+
     while (t--) {
-        ull n;
+        long long n;
         cin >> n;
         n %= MOD;
         cout << (n * n) % MOD << endl;
     }
     return 0;
 }
+/*
+ * ˜a‚Ì’†”²‚¯B
+ * T1=1^2-0^2
+ * T2=2^2-1^2
+ * ...
+ * Tn=n^2-(n-1)^2
+ * “š‚¦‚Ín^2
+ */
