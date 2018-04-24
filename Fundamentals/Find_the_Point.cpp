4a7a1d49b3f7af5322c25c5ce625492af3098e77
Hiroyuki Hirano <aitdccd@gmail.com>
2018-04-24 19:34:23 +0900

    Find the Point

--- Fundamentals/Find_the_Point.cpp
+++ Fundamentals/Find_the_Point.cpp
@@ -1,15 +1,25 @@
+// https://www.hackerrank.com/challenges/find-point/problem
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
     int n;
     cin >> n;
+
     while (n--) {
         int px, py, qx, qy;
         cin >> px >> py >> qx >> qy;
-        cout << 2 * qx - px << " " << 2 * qy - py << endl;
+
+        int rx = 2 * qx - px;
+        int ry = 2 * qy - py;
+        cout << rx << " " << ry << endl;
     }
     return 0;
 }
+
+/*
+ * qx=(px+rx)/2, qy=(py+ry)/2
+ * ˆ rx=2*qx-px, ry=2*qy-py
+ */
