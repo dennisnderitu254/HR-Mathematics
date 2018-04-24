ea9a27a1b3f23ead31999879155266bcc500b0d6
Hiroyuki Hirano <aitdccd@gmail.com>
2018-04-24 19:58:49 +0900

    Cutting Paper Squares

--- Fundamentals/Cutting_Paper_Squares.cpp
+++ Fundamentals/Cutting_Paper_Squares.cpp
@@ -1,14 +1,19 @@
+// https://www.hackerrank.com/challenges/p1-paper-cutting/problem
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
-#define ll long long
-// m-1+m(n-1)=mn-1
-
 int main() {
-    ios::sync_with_stdio(false);
-    ll n, m;
+    long long n, m;
     cin >> n >> m;
     cout << m * n - 1 << endl;
     return 0;
 }
+
+/*
+ * 縦m，横nだとする。
+ * まず横にm-1回切る。m個の紙片になる。
+ * 各紙片をn-1回ずつ切ればいいので
+ * m-1+m(n-1)=mn-1
+ */
